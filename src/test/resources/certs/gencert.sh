c77a9362d1cfcbb546503c1f9e563c6119cd8b10
Juan Medina <jamedina@gmail.com>
2019-03-26 22:02:19 +0000

    update cert generation for #4

--- src/test/resources/certs/gencert.sh
+++ src/test/resources/certs/gencert.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-## generate certs and keys for localhost testing, when asked set password as 'password' and cn as 'localhost'
-openssl req -newkey rsa:2048 -nodes -keyout private.pem -x509 -days 3650 -out certificate.pem
-openssl pkcs12 -inkey private.pem -in certificate.pem -name selfsigned -export -out certificate.p12
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+openssl req -newkey rsa:2048 -nodes -keyout private.pem -x509 -days 3650 -out certificate.pem -subj "/C=UK/ST=London/L=London/O=reasec/CN=localhost"
+openssl pkcs12 -inkey private.pem -in certificate.pem -name selfsigned -export -out certificate.p12 -password pass:password
 openssl x509 -pubkey -in certificate.pem -noout > public.pem
 rm private.pem
 rm certificate.pem
 
-## after this to import the p12 in cacerts, do this from the root of the project
-##
-##   sudo keytool -importkeystore -deststorepass changeit -noprompt -destkeystore ${JAVA_HOME}/jre/lib/security/cacerts -srckeystore src/test/resources/certs/certificate.p12 -srcstoretype pkcs12 -srcstorepass password -alias selfsigned
+set +e
+keytool -delete -storepass changeit -noprompt -keystore ${JAVA_HOME}/jre/lib/security/cacerts -alias selfsigned || true
+set -e
+keytool -importkeystore -deststorepass changeit -noprompt -destkeystore ${JAVA_HOME}/jre/lib/security/cacerts -srckeystore certificate.p12 -srcstoretype pkcs12 -srcstorepass password -alias selfsigned
+
+TOOLS_PATH="../../../../tools"
+
+if [ ! -f "$TOOLS_PATH/PublicKeySha.class" ]; then
+    javac "$TOOLS_PATH/PublicKeySha.java"
+fi
+
+PUBLIC_KEY_SHA="$(java -cp "$TOOLS_PATH" PublicKeySha public.pem | grep "sha:" | cut -d' ' -f2)"
+
+sed '$d' ../application.yml > ../application2.yml 
+echo "  public-key-sha: \"$PUBLIC_KEY_SHA\"" | cat ../application2.yml - > ../application.yml
+rm ../application2.yml
--- tools/PublicKeySha.java
+++ tools/PublicKeySha.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2019 The ReaSec project
  *
@@ -20,9 +21,13 @@ import java.security.MessageDigest;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import sun.misc.BASE64Decoder;
 
 /*
- * Simple program to get the public key sha from a giving site.
+ * Simple program to get the public key sha from a giving site or pem file.
  * This program depends only on the JDK classes and has not external
  * dependencies.
  *
@@ -33,6 +38,10 @@ import java.security.cert.X509Certificate;
  *   to run this program execute:
  *
  *     $ java PublicKeySha https://www.site.com
+ * 
+ *   or
+ * 
+ *     $ java PublicKeySha public-key.pem
  *
  *   the output of this program will be something like:
  *
@@ -50,8 +59,8 @@ public class PublicKeySha {
     }
   }
 
-  final static char[] lookupTable = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
-      'E', 'F'};
+  final static char[] lookupTable = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+      'E', 'F' };
 
   private static byte[] getSha256(final byte[] bytes) {
     synchronized (sha256md) {
@@ -76,38 +85,97 @@ public class PublicKeySha {
     return new String(hexChars);
   }
 
-  private static String getPublicKeySha(final X509Certificate certificate) {
-    final PublicKey publicKey = certificate.getPublicKey();
-    final byte[] encoded = publicKey.getEncoded();
-    final byte[] sha = getSha256(encoded);
+  private static String getPublicKeySha(byte[] publicKey) {
+    final byte[] sha = getSha256(publicKey);
     return encodeToHex(sha);
   }
 
-  private static X509Certificate getCertificate(final String uri) {
+  private static byte[] getPublicKeyFromURL(final String uri) {
+    HttpsURLConnection connection = null;
     try {
       final URL url = new URL(uri);
-      final HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+      connection = (HttpsURLConnection) url.openConnection();
+
       connection.connect();
       final Certificate[] serverCertificates = connection.getServerCertificates();
       final X509Certificate firstCertificate = (X509Certificate) serverCertificates[0];
       connection.disconnect();
-      return firstCertificate;
+      connection = null;
+
+      final PublicKey publicKey = firstCertificate.getPublicKey();
+      final byte[] encoded = publicKey.getEncoded();
+
+      return encoded;
     } catch (Exception e) {
-      throw new RuntimeException("Can't get certificate for URL: " + uri, e);
+      throw new RuntimeException("Can't get public key for URL: " + uri, e);
+    } finally {
+      if (connection != null) {
+        connection.disconnect();
+      }
     }
+  }
+
+  private static byte[] getPublicKeyFromPEM(final String filePath) {
+    try (final BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+      String strKeyPEM = "";
+      String line;
+      while ((line = br.readLine()) != null) {
+        strKeyPEM += line + "\n";
+      }
+      if ((strKeyPEM.contains("-----BEGIN PUBLIC KEY-----\n")) & (strKeyPEM.contains("-----END PUBLIC KEY-----"))) {
+        strKeyPEM = strKeyPEM.replace("-----BEGIN PUBLIC KEY-----\n", "");
+        strKeyPEM = strKeyPEM.replace("-----END PUBLIC KEY-----", "");
+
+        final BASE64Decoder b64 = new BASE64Decoder();
+        return b64.decodeBuffer(strKeyPEM);
+      } else {
+        throw new RuntimeException("pem not valid");
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("Can't get public key for pem: " + filePath, e);
+    }
+  }
+
+  public static boolean isURL(final String argument) {
+    if (argument.startsWith("http://")) {
+      throw new RuntimeException("Couldn't get a certificate from a http site: " + argument);
+    }
+    return argument.startsWith("https://");
+  }
+
+  private static byte[] getPublicKey(final String parameter) {
+    final byte[] publicKey;
+
+    if (isURL(parameter)) {
+      publicKey = getPublicKeyFromURL(parameter);
+    } else {
+      publicKey = getPublicKeyFromPEM(parameter);
+    }
+
+    if (publicKey == null) {
+      throw new RuntimeException("Can't get public key for: " + parameter);
+    }
+
+    return publicKey;
   }
 
   public static void main(String[] args) {
     try {
+      if (sha256md == null) {
+        throw new RuntimeException("We couldn't create sha256");
+      }
       if (args.length == 0) {
-        System.out.println("Error: Missing URL to get public key sha.");
-        System.out.println(" usage:");
+        System.out.println("Error: Missing parameter to get public key sha.");
+        System.out.println(" usages:");
         System.out.println("   java PublicKeySha https://www.site.com");
+        System.out.println("   java PublicKeySha public-key.pem");
       } else {
-        final String uri = args[0];
-        System.out.println("Getting public key sha for: " + uri);
-        final X509Certificate certificate = getCertificate(args[0]);
-        final String sha = getPublicKeySha(certificate);
+        final String parameter = args[0];
+        System.out.println("Getting public key sha for: " + parameter);
+
+        final byte[] publicKey = getPublicKey(parameter);
+        final String sha = getPublicKeySha(publicKey);
+
         System.out.println("sha: " + sha);
       }
     } catch (Exception ex) {

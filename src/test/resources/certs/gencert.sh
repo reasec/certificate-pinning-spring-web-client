127d5fd6ea2e4354028068b8133f1a1197a3e347
Juan Medina <jamedina@gmail.com>
2019-03-27 08:16:09 +0000

    separate generate and import

--- src/test/resources/certs/gencert.sh
+++ src/test/resources/certs/gencert.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
-#we need root for doing this
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+#
+# Copyright 2019 The ReaSec project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+############################################################################
+## script : gencert.sh
+##
+## generate a p12 certificate, a public key and update application.yml
+############################################################################
 
 #generate certificate, private key, public key and p12
 openssl req -newkey rsa:2048 -nodes -keyout private.pem -x509 -days 3650 -out certificate.pem -subj "/C=UK/ST=London/L=London/O=reasec/CN=localhost"
@@ -12,24 +31,16 @@ openssl x509 -pubkey -in certificate.pem -noout > public.pem
 rm private.pem
 rm certificate.pem
 
-#ignore errors and remove the certificate from keystore
-set +e
-keytool -delete -storepass changeit -noprompt -keystore ${JAVA_HOME}/jre/lib/security/cacerts -alias selfsigned || true
-set -e
-
-#import certificate in the key store
-keytool -importkeystore -deststorepass changeit -noprompt -destkeystore ${JAVA_HOME}/jre/lib/security/cacerts -srckeystore certificate.p12 -srcstoretype pkcs12 -srcstorepass password -alias selfsigned
-
 #set our tools folder
 TOOLS_PATH="../../../../tools"
 
-#Compile PublicKeySha if required
-if [ ! -f "$TOOLS_PATH/PublicKeySha.class" ]; then
-    javac "$TOOLS_PATH/PublicKeySha.java"
-fi
+#Compile PublicKeySha
+javac "$TOOLS_PATH/PublicKeySha.java"
 
 #Get the public key sha
 PUBLIC_KEY_SHA="$(java -cp "$TOOLS_PATH" PublicKeySha public.pem | grep "sha:" | cut -d' ' -f2)"
 
 #change our test yaml with the new sha
 sed "s/NEWPUBLICKEY/$PUBLIC_KEY_SHA/g" ../application.tpl.yml > ../application.yml
+
+echo "p12 generated an application.yml update with the public key sha, import it into the Java Key Store with import.sh"
